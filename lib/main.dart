e47960d5a102474a145e4b1c65884771fa6a30a5
Roger Florat <roger.florat@avanade.com>
2024-07-24 00:17:50 +0200

    Setup Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:t_store/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:t_store/utils/theme/theme.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  // Todo: Add Widgets Binding
+  // Todo: Init Local Storage
+  // Todo: Init Payment Methods
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
+  // Todo: Initialize Authentication
+
+  // Load all the Material Design / Themes / Localizations / Bindings
+
   runApp(const MainApp());
 }
 
