e7cf3bd29fa31d29560fb517afcbf0de2f972e35
Roger Florat <roger.florat@avanade.com>
2024-07-29 18:11:32 +0200

    New Assets

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,59 +1,95 @@
 class TImages {
-  TImages._();
-
-  // -- App Logos
+  // -- App Logo
   static const String darkAppLogo =
       "assets/logos/t-store-splash-logo-black.png";
   static const String lightAppLogo =
       "assets/logos/t-store-splash-logo-white.png";
 
-  // -- Social Logos
+  // -- Social Logo
   static const String google = "assets/logos/google-icon.png";
   static const String facebook = "assets/logos/facebook-icon.png";
 
-  // -- Onboarding Image
+  // -- OnBoarding Screen
   static const String onBoardingImage1 =
-      "assets/images/on_boarding_images/sammy-line-searching.gif";
-  static const String onBoardingImage2 =
       "assets/images/on_boarding_images/sammy-line-shopping.gif";
+  static const String onBoardingImage2 =
+      "assets/images/on_boarding_images/sammy-line-searching.gif";
   static const String onBoardingImage3 =
-      "assets/images/on_boarding_images/sammy-line-delivery.gif";
+      "assets/images/on_boarding_images/sammy-line-delivering.gif";
 
   // -- Animations
   static const String productsIllustration =
-      "assets/images/animations/sammy-line-workout.gif";
+      "assets/images/animation/sammy-line-workout.gif";
   static const String productsSaleIllustration =
-      "assets/images/animations/sammy-line-sale.png";
+      "assets/images/animation/sammy-line-sale.png";
   static const String staticSuccessIllustration =
-      "assets/images/animations/sammy-line-success.png";
+      "assets/images/animation/sammy-line-man-and-woman-high-fiving-each-other.png";
   static const String deliveredInPlaneIllustration =
-      "assets/images/animations/sammy-line-come-back-later.png";
+      "assets/images/animation/sammy-line-come-back-latter.png";
   static const String deliveredEmailIllustration =
-      "assets/images/animations/sammy-line-man-receives-a-mail.png";
+      "assets/images/animation/sammy-line-man-receives-a-mail.png";
   static const String verifyIllustration =
-      "assets/images/animations/sammy-line-travel-backpack-with-passport-and-air-ticket.gif";
+      "assets/images/animation/sammy-line-man-receives-a-mail.png";
+  static const String loading = "assets/images/animation/process.gif";
 
-  // -- Category Icons
+  // category Icons
   static const String sportIcon =
-      "assets/icons/categories/icons8-bowling-64.png";
-  static const String clothIcon = "assets/icons/categories/cloth-icon.png";
+      "assets/images/category/sammy-line-workout.gif";
+  static const String shoesIcon = "assets/icons/category/icons8-shoes-50.png";
+  static const String bowlingIcon =
+      "assets/icons/category/icons8-bowling-64.png";
 
-// -- Banners
-  static const String promoBanner1 = "assets/images/banners/promo-banner-1.png";
-  static const String promoBanner2 = "assets/images/banners/promo-banner-2.png";
-  static const String promoBanner3 = "assets/images/banners/promo-banner-3.png";
+  //Banner
+  static const String promoBanner1 = "assets/images/banner/promobanner.jpg";
+  static const String promoBanner2 = "assets/images/banner/promobanner.jpg";
+  static const String promoBanner3 = "assets/images/banner/promobanner.jpg";
 
-// -- Profile
+  // user
   static const String user = "assets/images/user/user.png";
 
-  // -- Products
-  static const String productImage1 = "assets/images/products/nike-shoes.png";
-  static const String productImage2 = "assets/images/products/nike-shoes.png";
+  // -- Product
+  static const String productImage1 =
+      "assets/images/products/imgbin_air-force-1-nike-air-max-air-jordan-sports-shoes-png.png";
+  static const String productImage2 =
+      "assets/images/products/imgbin_sports-shoes-air-force-1-nike-air-max-png.png";
   static const String productImage3 =
-      "assets/images/products/product-jacket.jpg";
+      "assets/images/products/kisspng-argentina-national-football-team-fifa-world-cup-fc-5b10a28af13d24.6674169515278168429881.png";
+  static const String productImage4 =
+      "assets/images/products/kisspng-download-parachute-icon-hot-air-balloon-5a9d49fbcb68e8.8858132815202575318332.png";
+  static const String productImage5 =
+      "assets/images/products/kisspng-iphone-5s-iphone-4s-iphone-6-iphone-8-plus-black-iphone-7-png-5ab187f2424456.2816668015215841142714.png";
+  static const String productImage6 =
+      "assets/images/products/transparent-hot-air-balloon-60abffca6cf390.4553580416218848744463.png";
+  static const String productImage7 =
+      "assets/images/products/transparent-pink-flower-pink-flower-in-full-bloom-with-green-leaves65a8b20a72c3c8.0502971617055544424701.png";
+  static const String productImage8 =
+      "assets/images/products/imgbin_air-force-1-nike-air-max-air-jordan-sports-shoes-png.png";
+  static const String productImage9 =
+      "assets/images/products/imgbin_sports-shoes-air-force-1-nike-air-max-png.png";
+  static const String productImage10 =
+      "assets/images/products/kisspng-argentina-national-football-team-fifa-world-cup-fc-5b10a28af13d24.6674169515278168429881.png";
+  static const String productImage11 =
+      "assets/images/products/kisspng-download-parachute-icon-hot-air-balloon-5a9d49fbcb68e8.8858132815202575318332.png";
+  static const String productImage12 =
+      "assets/images/products/kisspng-iphone-5s-iphone-4s-iphone-6-iphone-8-plus-black-iphone-7-png-5ab187f2424456.2816668015215841142714.png";
+  static const String productImage13 =
+      "assets/images/products/transparent-hot-air-balloon-60abffca6cf390.4553580416218848744463.png";
+  static const String productImage14 =
+      "assets/images/products/transparent-pink-flower-pink-flower-in-full-bloom-with-green-leaves65a8b20a72c3c8.0502971617055544424701.png";
+  static const String productImage15 =
+      "assets/images/products/imgbin_air-force-1-nike-air-max-air-jordan-sports-shoes-png.png";
+  static const String productImage16 =
+      "assets/images/products/imgbin_sports-shoes-air-force-1-nike-air-max-png.png";
+  static const String productImage17 =
+      "assets/images/products/kisspng-argentina-national-football-team-fifa-world-cup-fc-5b10a28af13d24.6674169515278168429881.png";
+  static const String productImage18 =
+      "assets/images/products/kisspng-download-parachute-icon-hot-air-balloon-5a9d49fbcb68e8.8858132815202575318332.png";
+  static const String productImage19 =
+      "assets/images/products/kisspng-iphone-5s-iphone-4s-iphone-6-iphone-8-plus-black-iphone-7-png-5ab187f2424456.2816668015215841142714.png";
+  static const String productImage20 =
+      "assets/images/products/transparent-hot-air-balloon-60abffca6cf390.4553580416218848744463.png";
+  static const String productImage21 =
+      "assets/images/products/transparent-pink-flower-pink-flower-in-full-bloom-with-green-leaves65a8b20a72c3c8.0502971617055544424701.png";
 
-  // -- Payment Methods
   static const String paypal = "assets/icons/payment_methods/paypal.png";
-  static const String successfulPaymentIcon =
-      "assets/icons/payment_methods/successful.png";
 }
